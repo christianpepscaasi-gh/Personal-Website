57fc5105b9cb245421138d5106c76fb523b54507
christianpepscaasi-gh <christianpepscaasi@gmail.com>
2026-09-24 15:18:46 +0800

    initial css fix and change to hex number

--- v1/script.js
+++ v1/script.js
@@ -4,10 +4,13 @@ let counter = 1;
 
 const name = "Peps";
 
+// get the element of the id named button1 and button_target
+// add and Event Listener on the click and change the text of the target
 const clickButton = document.getElementById("button1");
 const buttonTarget = document.getElementById("button_target");
 clickButton.addEventListener("click", () => {
     buttonTarget.textContent = "You clicked me!";
-    buttonTarget.style.color = "red";
+    buttonTarget.style.color = "#fbbf24";
 }
 );
+
